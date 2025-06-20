70f57553f963c21c056f144b4b9a934392367ee7
Chris Pelatari <chris@bluefenix.net>
2025-06-20 16:34:51 -0500

    add gh-contrib-graph

--- src/views/WhatIKnow.vue
+++ src/views/WhatIKnow.vue
@@ -7,7 +7,9 @@
         <Heading cols="auto">
           What I know.
         </Heading>
-
+        <div id="gh" data-login="ChrisPelatari">
+          <a href='https://hachyderm.io/@blue_fenix' alt="blue_fenix on Mastodon" rel="me"> <svg xmlns="http://www.w3.org/2000/svg" height="24" width="21" viewBox="0 0 448 512"><path fill="#7f7f7f" d="M433 179.1c0-97.2-63.7-125.7-63.7-125.7-62.5-28.7-228.6-28.4-290.5 0 0 0-63.7 28.5-63.7 125.7 0 115.7-6.6 259.4 105.6 289.1 40.5 10.7 75.3 13 103.3 11.4 50.8-2.8 79.3-18.1 79.3-18.1l-1.7-36.9s-36.3 11.4-77.1 10.1c-40.4-1.4-83-4.4-89.6-54a102.5 102.5 0 0 1 -.9-13.9c85.6 20.9 158.7 9.1 178.8 6.7 56.1-6.7 105-41.3 111.2-72.9 9.8-49.8 9-121.5 9-121.5zm-75.1 125.2h-46.6v-114.2c0-49.7-64-51.6-64 6.9v62.5h-46.3V197c0-58.5-64-56.6-64-6.9v114.2H90.2c0-122.1-5.2-147.9 18.4-175 25.9-28.9 79.8-30.8 103.8 6.1l11.6 19.5 11.6-19.5c24.1-37.1 78.1-34.8 103.8-6.1 23.7 27.3 18.4 53 18.4 175z"/></svg></a>
+        </div>
         <v-col cols="auto">
           <blockquote style="max-width: 230px;">
             <p class="font-weight-light font-italic">
@@ -38,7 +40,390 @@
 </template>
 
 <script setup lang="ts">
-  import { useAppStore } from '@/stores/app'
+import { onMounted } from 'vue'
+import { useAppStore } from '@/stores/app'
 
-  const { schema } = useAppStore()
+const { schema } = useAppStore()
+
+onMounted(() => {
+  main()
+})
+
+async function fetchData(ghLogin: string | undefined) { 
+  const data = 
+        await fetch('https://api.github.com/graphql', {
+          method: 'POST',
+          headers: {
+            'Authorization': `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            query: `
+              query($login: String!) {
+                user(login: $login) {
+                  contributionsCollection {
+                    contributionCalendar {
+                      totalContributions
+                      months {
+                        name
+                        totalWeeks
+                      }
+                      weeks {
+                        contributionDays {
+                          date
+                          contributionCount
+                          contributionLevel
+                          weekday
+                        }
+                      }
+                      colors
+                    }
+                  }
+                  avatarUrl
+                }
+              }
+            `,
+            variables: {
+              login: ghLogin
+            }
+          })
+        })
+        .then(response => response.json())
+        .then(data => data)
+        .catch(error => console.error('Error fetching data:', error));
+
+  return data["data"]["user"]
+}
+
+function init_table() {
+  let table = document.createElement("table")
+  table.className = "ghCalendarTable"
+  let thead = table.createTHead()
+  let tbody = table.createTBody()
+  let row = thead.insertRow()
+  let cell = row.insertCell()
+  cell.style.width = "28px"
+  for (let i = 0; i < 7; i++) {
+    const row = tbody.insertRow()
+    const cell = row.insertCell()
+    switch (i) {
+      case 1: cell.innerHTML = '<span class="ghCalendarLabel">Mon</span>'
+      break
+      case 3: cell.innerHTML = '<span class="ghCalendarLabel">Wed</span>'
+      break
+      case 5: cell.innerHTML = '<span class="ghCalendarLabel">Fri</span>'
+      break
+    }
+  }
+  return [table, thead, tbody]
+}
+
+function addMonths(thead: HTMLTableElement | HTMLTableSectionElement, months: string | any[]) {
+  for (let i = 0; i < months.length; i++) {
+    if (months[i]["totalWeeks"] >= 2) {
+      let cell = thead.rows[0].insertCell()
+      let label = document.createElement("span")
+      label.textContent = months[i]["name"]
+      label.className = "ghCalendarLabel"
+      cell.appendChild(label)
+      cell.colSpan = months[i]["totalWeeks"]
+    }
+  }
+}
+
+function addWeeks(tbody: HTMLTableElement | HTMLTableSectionElement, weeks: string | any[], colors: any) {
+  for (let i = 0; i < weeks.length; i++) {
+    const days = weeks[i]["contributionDays"]
+    for (let j = 0; j < days.length; j++) {
+      const day = days[j]
+      const data = document.createElement("span")
+      const date = new Date(day["date"])
+      data.textContent = `${day["contributionCount"]} contributions on ${date.toDateString()}`
+      const cell = tbody.rows[day["weekday"]].insertCell()
+      cell.appendChild(data)
+      cell.className = "ghCalendarDayCell"
+      cell.dataset.date = day["date"]
+      cell.dataset.count = day["contributionCount"]
+      cell.dataset.level = day["contributionLevel"]
+    }
+  }
+}
+
+function init_card() {
+  const card = document.createElement("div")
+  card.className = "ghCalendarCard"
+  return card
+}
+
+function init_card_footer() {
+  const footer = document.createElement("div")
+  const link = document.createElement("a")
+  const colors = document.createElement("div")
+  footer.className = "ghCalendarCardFooter"
+  link.className = "ghCalendarCardFooterLink"
+  colors.className = "ghCalendarCardFooterColors"
+  let less = document.createElement("span")
+  less.textContent = "Less"
+  let more = document.createElement("span")
+  more.textContent = "More"
+  colors.appendChild(less)
+  let levels = ["NONE", "FIRST_QUARTILE", "SECOND_QUARTILE", "THIRD_QUARTILE", "FOURTH_QUARTILE"]
+  for (let i = 0; i < 5; i++) {
+    let cell = document.createElement("div")
+    cell.className = "ghCalendarDayCell"
+    cell.dataset.level = levels[i]
+    colors.appendChild(cell)
+  }
+  colors.appendChild(more)
+  footer.appendChild(link)
+  footer.appendChild(colors)
+  return footer
+}
+
+function init_canvas() {
+  const canvas = document.createElement("div")
+  canvas.className = "ghCalendarCanvas"
+  return canvas
+}
+
+function init_header(total_contribs: any, ghLogin: string | undefined, avatarUrl: any) {
+  const header = document.createElement("div")
+  const total = document.createElement("span")
+  const profile = document.createElement("div")
+  profile.innerHTML = `<a href="https://github.com/${ghLogin}">${ghLogin}</a><img src="${avatarUrl}">`
+  header.className = "ghCalendarHeader"
+  total.textContent = `${total_contribs} contributions in the last year`
+  header.appendChild(total)
+  header.appendChild(profile)
+  return header
+}
+
+function init_thumbnail() {
+    const thumbnail = document.createElement("div")
+    const thumbNailLink = document.createElement("a")
+    const thumbNailIcon = document.createElement("span")
+    const thumbNailText = document.createElement("span")
+
+    thumbnail.className = "ghThumbNail"
+    thumbNailLink.href = "https://github.com/ChrisPelatari/gh-contrib-graph"
+    //use vitepress github icon
+    thumbNailIcon.textContent = "";
+    thumbNailIcon.className = "mdi-github mdi v-icon"
+    //link to gh-contrib-graph
+    thumbNailText.textContent = "gh-contrib-graph"
+    //thumbNailText.style.marginLeft = "-13px"
+    thumbNailLink.appendChild(thumbNailIcon)
+    thumbNailLink.appendChild(thumbNailText)
+    thumbnail.appendChild(thumbNailLink)
+    return thumbnail
+}
+
+async function main() {
+  const container = document.getElementById("gh")
+  if (!container) {
+    console.error("Container element not found");
+    return;
+  }
+  const ghLogin = container.dataset.login;
+  const data = await fetchData(ghLogin)
+  const calendar = data["contributionsCollection"]["contributionCalendar"]
+  const [table, thead, tbody] = init_table()
+  const card = init_card()
+  const canvas = init_canvas()
+  const header = init_header(data["contributionsCollection"]["contributionCalendar"]["totalContributions"], ghLogin, data["avatarUrl"])
+  const footer = init_card_footer()
+  const thumbnail = init_thumbnail()
+
+  addWeeks(tbody, calendar["weeks"], calendar["colors"])
+  addMonths(thead, calendar["months"])
+  canvas.appendChild(table)
+  canvas.appendChild(footer)
+  card.appendChild(canvas)
+  container.appendChild(header)
+  container.appendChild(card)
+  container.appendChild(thumbnail)
+}
 </script>
+<style scoped>
+#gh {
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 2.5rem;
+}
+:root {
+    --gh-bg-color: #0d1117;
+    --gh-base-size-4: 0.25rem;
+    --gh-base-size-8: 0.5rem;
+    --gh-base-size-12: 0.75rem;
+    --gh-base-size-16: 1rem;
+    --gh-base-size-32: 2rem;
+    --gh-cell-level0-color: #21262d;
+    --gh-cell-level1-color: #0e4429;
+    --gh-cell-level2-color: #006d32;
+    --gh-cell-level3-color: #26a641;
+    --gh-cell-level4-color: #39d353;
+    --gh-cell-info-bg-color: #6e7681;
+    --gh-cell-outline-color: #ffffff0d;
+    --gh-cell-border-color: rgba(255, 255, 255, 0.03);
+    --gh-border-card-color: #30363d;
+    --gh-border-card-width: max(1px, 0.0625rem);
+    --gh-text-default-color: #e6edf3;
+    --gh-text-inactive-color: #848D97;
+    --gh-link-hover-color: #2f81f7;
+    --gh-font-default-family: -apple-system,BlinkMacSystemFont,"Segoe UI","Noto Sans",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji";
+}
+
+/* Main block */
+#gh {
+    color: var(--gh-text-default-color);
+    width: fit-content;
+    font-family: var(--gh-font-default-family);
+}
+#gh a {
+    text-decoration: none;
+    color: var(--gh-text-inactive-color)
+}
+#gh a:hover {
+    color: var(--gh-link-hover-color);
+}
+
+/* Table */
+.ghCalendarTable {
+    color: var(--gh-text-default-color);
+    border-spacing: 3px;
+    border-collapse: separate;  
+}
+.ghCalendarTable tr {
+    height: 10px;
+}
+.ghCalendarTable td {
+    text-align: left;
+    box-sizing: border-box;
+    position: relative;
+}
+
+/* DayCell */
+.ghCalendarDayCell {
+    width: 10px;
+    border-radius: 2px;
+    border: 1px solid var(--gh-cell-border-color);
+    outline: 1px solid var(--gh-cell-outline-color);
+    outline-offset: -2px;
+}
+.ghCalendarDayCell[data-level="NONE"] {
+    background-color: var(--gh-cell-level0-color);
+    border: none;
+    outline: none;
+}
+.ghCalendarDayCell[data-level="FIRST_QUARTILE"] {
+    background-color: var(--gh-cell-level1-color);
+}
+.ghCalendarDayCell[data-level="SECOND_QUARTILE"] {
+    background-color: var(--gh-cell-level2-color);
+}
+.ghCalendarDayCell[data-level="THIRD_QUARTILE"] {
+    background-color: var(--gh-cell-level3-color);
+}
+.ghCalendarDayCell[data-level="FOURTH_QUARTILE"] {
+    background-color: var(--gh-cell-level4-color);
+}
+.ghCalendarDayCell span {
+    visibility: hidden;
+    position: absolute;
+    transform: translate(-50%, -130%);
+    word-wrap: normal;
+    text-wrap: nowrap;
+    pointer-events: none;
+    background-color: var(--gh-cell-info-bg-color);
+    padding: 10px;
+    font-size: 12px;
+    border-radius: 6px;
+    font-family: var(--gh-font-default-family);
+}
+.ghCalendarDayCell span::after {
+    content: "";
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    border-width: 5px;
+    border-style: solid;
+    border-color: var(--gh-cell-info-bg-color) transparent transparent transparent;
+}
+.ghCalendarDayCell:hover span {
+    visibility: visible;
+}
+.ghCalendarLabel {
+    font-size: 12px;
+    position: absolute;
+    bottom: -3px;
+    font-weight: 400;
+    font-family: var(--gh-font-default-family);
+}
+
+/* Containers */
+.ghCalendarCard {
+    background-color: var(--gh-bg-color);
+    padding: var(--gh-base-size-16);
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    border-top-left-radius: 6px;
+    border-top-right-radius: 6px;
+    border: var(--gh-border-card-width, 1px) solid var(--gh-border-card-color);
+}
+.ghCalendarCanvas {
+    display: flex !important;
+    flex-direction: column;
+    padding-top: var(--gh-base-size-8, 4px) !important;
+    text-align: center !important;
+    margin-right: var(--gh-base-size-8, 8px) !important;
+    margin-left: var(--gh-base-size-8, 8px) !important;
+    overflow: visible;
+}
+
+/* Header */
+.ghCalendarHeader {
+    margin-bottom: var(--gh-base-size-4);
+    height: 20px;
+}
+.ghCalendarHeader span {
+    float: left;
+}
+.ghCalendarHeader div {
+    display: flex;
+    flex-direction: row;
+    float: right;
+}
+.ghCalendarHeader div img {
+    width: 20px;
+    height: 20px;
+    border-radius: 10px;
+    margin-left: var(--gh-base-size-4);
+}
+
+/* Footer */
+.ghCalendarCardFooter {
+    display: block;
+    padding: var(--gh-base-size-4, 4px) var(--gh-base-size-32, 32px) !important;
+    text-align: center !important;
+    font-size: var(--gh-base-size-12);
+    font-family: var(--gh-font-default-family);
+    color: var(--gh-text-inactive-color);
+}
+.ghCalendarCardFooterLink {
+    display: block;
+    float: left;
+}
+.ghCalendarCardFooterColors {
+    display: flex;
+    flex-direction: row;
+    float: right;
+    align-items: center;
+}
+.ghCalendarCardFooterColors * {
+    margin-right: var(--gh-base-size-4);
+}
+.ghCalendarCardFooterColors div {
+    width: 10px;
+    height: 10px;
+}
+</style>
